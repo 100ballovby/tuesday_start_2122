35ad72afdce5b76be73bde1254a0ec7ba908f7c4
demidraksin <greatraksin@icloud.com>
2021-10-12 17:25:04 +0300

    Adding libraries reference

---
+++ 03_lesson_1210/02_random_library.py
@@ -0,0 +1,6 @@
+import random as r
+
+print(r.randint(1, 5))  # random integer
+print(r.uniform(10.0, 50.0))
+print(r.random())  # десятичные числа от 0 до 1
+print(r.choice('мама мыла раму где мама'))
---
+++ 03_lesson_1210/05_string_library.py
@@ -0,0 +1,10 @@
+import string as s
+
+print(f'Все буквы английского алфавита: {s.ascii_letters}')
+print(f'Маленькие буквы английского алфавита: {s.ascii_lowercase}')
+print(f'Большие буквы английского алфавита: {s.ascii_uppercase}')
+print(f'Цифры: {s.digits}')
+print(f'HEX-алфавит: {s.hexdigits}')
+print(f'Спец.символы: {s.punctuation}')
+
+
