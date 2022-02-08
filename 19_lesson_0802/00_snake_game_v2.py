9e7257734f1f00ce84e2ae2642510e7d3e42447c
demidraksin <greatraksin@icloud.com>
2022-02-08 17:04:41 +0300

    Finishing snake game v2

--- 19_lesson_0802/00_snake_game_v2.py
+++ 19_lesson_0802/00_snake_game_v2.py
@@ -13,16 +13,6 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 74, 74)
 
-x1 = 200  # змея появляется в этих координатах
-y1 = 200  # змея появляется в этих координатах
-x1_change = 0  # изменение положения змеи в пространстве
-y1_change = 0  # изменение положения змеи в пространстве
-snake_block = 30  # размер змеи
-speed = 5
-
-food_x = round(randrange(0, W - snake_block) / 10) * 10  # случайные координаты появления еды
-food_y = round(randrange(0, H - snake_block) / 10) * 10  # случайные координаты появления еды
-
 
 def show_message(text, color, x, y):
     pg.font.init()  # чтобы работали надписи на экране
@@ -36,68 +26,84 @@ def snake_draw(block, s_list):
         snake = rect(screen, BLUE, [x[0], x[1], block, block])
     return snake
 
-snake_list = []
-snake_length = 1
 
-finished = False
-pause = False
-while not finished:
-    while pause:  # пока игра стоит "на паузе"
+def game_loop():
+    x1 = 200  # змея появляется в этих координатах
+    y1 = 200  # змея появляется в этих координатах
+    x1_change = 0  # изменение положения змеи в пространстве
+    y1_change = 0  # изменение положения змеи в пространстве
+    snake_block = 30  # размер змеи
+    speed = 5
+
+    food_x = round(randrange(0, W - snake_block) / 10) * 10  # случайные координаты появления еды
+    food_y = round(randrange(0, H - snake_block) / 10) * 10  # случайные координаты появления еды
+
+    snake_list = []
+    snake_length = 1
+
+    finished = False
+    pause = False
+    while not finished:
+        while pause:  # пока игра стоит "на паузе"
+            screen.fill(WHITE)
+            show_message('Нажмите C, чтобы продолжить, или ESC, чтобы выйти', BLACK, 200, 250)
+            pg.display.update()
+
+            # цикл обработки событий в паузе
+            for pause_event in pg.event.get():
+                if pause_event.type == pg.KEYDOWN:
+                    if pause_event.key == pg.K_ESCAPE:  # если нажали ESC, завершить игру
+                        pause = False  # выключить паузу
+                        finished = True  # выключить игру
+                    elif pause_event.key == pg.K_c:  # если нажали С, "начать игру заново"
+                        game_loop()
+
+        clock.tick(speed)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                finished = True
+            elif event.type == pg.KEYDOWN:  # если кто-то нажал на кнопку
+                if event.key == pg.K_RIGHT:  # pg.K_d  wasd
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pg.K_LEFT:  # pg.K_a
+                    x1_change = -snake_block  # сдвигаю змею по горизонтали на расстояние, равное ее размеру
+                    y1_change = 0
+                elif event.key == pg.K_UP:  # pg.K_w
+                    x1_change = 0
+                    y1_change = -snake_block  # сдвигаю змею по вертикали на расстояние, равное ее размеру
+                elif event.key == pg.K_DOWN:  # pg.K_s
+                    x1_change = 0
+                    y1_change = snake_block
+
+        if (x1 >= W or x1 < 0) or (y1 >= H or y1 < 0):  # если коснулись стены
+            pause = True  # остановить игру
+
+        x1 += x1_change
+        y1 += y1_change
         screen.fill(WHITE)
-        show_message('Нажмите C, чтобы продолжить, или ESC, чтобы выйти', BLACK, 200, 250)
+
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_list.append(snake_head)
+        if len(snake_list) > snake_length:
+            snake_list.pop(0)
+
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                pause = True
+
+        snake = snake_draw(snake_block, snake_list)
+        food = rect(screen, RED, [food_x, food_y, snake_block, snake_block])
+
+        show_message(f'Your score {snake_length - 1}', BLACK, 0, 0)
+
         pg.display.update()
 
-        # цикл обработки событий в паузе
-        for pause_event in pg.event.get():
-            if pause_event.type == pg.KEYDOWN:
-                if pause_event.key == pg.K_ESCAPE:  # если нажали ESC, завершить игру
-                    pause = False  # выключить паузу
-                    finished = True  # выключить игру
-                elif pause_event.key == pg.K_c:  # если нажали С, "начать игру заново"
-                    x1 = 100
-                    y1 = 100
-                    pause = False
+        if snake.colliderect(food):  # если змейка касается еды
+            snake_length += 1  # увеличиваю количество сегментов змеи
+            food_x = round(randrange(0, W - snake_block) / 10) * 10  # случайные координаты появления еды
+            food_y = round(randrange(0, H - snake_block) / 10) * 10
 
-    clock.tick(speed)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            finished = True
-        elif event.type == pg.KEYDOWN:  # если кто-то нажал на кнопку
-            if event.key == pg.K_RIGHT:  # pg.K_d  wasd
-                x1_change = snake_block
-                y1_change = 0
-            elif event.key == pg.K_LEFT:  # pg.K_a
-                x1_change = -snake_block  # сдвигаю змею по горизонтали на расстояние, равное ее размеру
-                y1_change = 0
-            elif event.key == pg.K_UP:  # pg.K_w
-                x1_change = 0
-                y1_change = -snake_block  # сдвигаю змею по вертикали на расстояние, равное ее размеру
-            elif event.key == pg.K_DOWN:  # pg.K_s
-                x1_change = 0
-                y1_change = snake_block
-
-    if (x1 >= W or x1 < 0) or (y1 >= H or y1 < 0):  # если коснулись стены
-        pause = True  # остановить игру
-
-    x1 += x1_change
-    y1 += y1_change
-    screen.fill(WHITE)
-
-    snake_head = []
-    snake_head.append(x1)
-    snake_head.append(y1)
-    snake_list.append(snake_head)
-    if len(snake_list) > snake_length:
-        snake_list.pop(0)
-
-    snake = snake_draw(snake_block, snake_list)
-    food = rect(screen, RED, [food_x, food_y, snake_block, snake_block])
-
-    show_message(f'Your score {snake_length - 1}', BLACK, 0, 0)
-
-    pg.display.update()
-
-    if snake.colliderect(food):  # если змейка касается еды
-        snake_length += 1  # увеличиваю количество сегментов змеи
-        food_x = round(randrange(0, W - snake_block) / 10) * 10  # случайные координаты появления еды
-        food_y = round(randrange(0, H - snake_block) / 10) * 10
+game_loop()
