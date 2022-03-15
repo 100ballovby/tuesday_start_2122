c64493841d3452fc7542d56e462bb03c17ffa312
demidraksin <greatraksin@icloud.com>
2022-03-15 16:33:48 +0300

    Adding game objects and visuals

--- 23_lesson_1503/00_racing_start.py
+++ 23_lesson_1503/00_racing_start.py
@@ -14,7 +14,9 @@ screen = pg.display.set_mode((W, H))
 clock = pg.time.Clock()
 
 # Game objects
-
+road = pg.Rect(170, 0, 300, H)
+border_left = pg.Rect(130, 0, 40, H)
+border_right = pg.Rect(470, 0, 40, H)
 
 finished = False
 while not finished:
@@ -22,6 +24,13 @@ while not finished:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
+
+    # Visuals
+    screen.fill(GREEN)  # фон игры (трава)
+    rect(screen, GRAY, road)  # дорога
+    rect(screen, SAND, border_left)  # левая обочина
+    rect(screen, SAND, border_right)  # правая обочина
+    rect(screen, WHITE, [316, 0, 8, H])  # линия разметки дороги
 
     # рисуем тут
     pg.display.update()
