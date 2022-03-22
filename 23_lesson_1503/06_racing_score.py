3a77674644ad9d63023ec5aa09a301585442bb88
demidraksin <greatraksin@icloud.com>
2022-03-22 17:15:57 +0300

    Adding score counting

--- 23_lesson_1503/06_racing_score.py
+++ 23_lesson_1503/06_racing_score.py
@@ -20,11 +20,28 @@ def moving(obj, speed, l_side, r_side):
         obj.left = l_side.right + 5  # отодвинуть машину от левой обочины
 
 
+def show_text(surf, text, x, y, size, col):
+    """
+    Функция будет отображать текст на экране.
+    :param surf: Поверхность, на которой отображается текст
+    :param text: текст
+    :param x: координаты отображения
+    :param y: координаты отображения
+    :param size: размер шрифта
+    :param col: цвет
+    :return: None
+    """
+    pg.font.init()
+    font = pg.font.SysFont('comicsans', size)
+    txt = font.render(f'{text}', True, col)
+    surf.blit(txt, [x, y])
+
 WHITE = (255, 255, 255)
 GREEN = (13, 145, 59)
 GRAY = (64, 64, 64)
 SAND = (255, 244, 189)
 RED = (209, 23, 39)
+ORANGE = (252, 186, 3)
 
 W = 800
 H = 900
@@ -37,6 +54,7 @@ road = pg.Rect(0, 0, W // 2, H)
 road.center = W // 2, H // 2
 border_left = pg.Rect(road.x - W * 0.1, 0, W * 0.1, H)
 border_right = pg.Rect(road.right, 0, W * 0.1, H)
+score_rect = pg.Rect(20, 20, W * 0.05, H * 0.05)
 
 road_width = road.width  # сохраняю ширину дороги
 paddle = pg.Rect(road.x, -30, road_width * 0.3, 30)
@@ -66,6 +84,7 @@ tree2_rect.y = 0 - H // 2
 
 car_speed = 0  # начальная скорость машины
 angle = 0  # угол поворота машины
+score = 0  # количество очков в начале игры
 
 finished = False
 while not finished:
@@ -98,6 +117,8 @@ while not finished:
     rect(screen, WHITE, [(W // 2) - (W * 0.01), 0, W * 0.02, H])  # линия разметки дороги
     rect(screen, RED, paddle)  # препятствие № 1
     rect(screen, RED, paddle2)  # препятствие № 2
+    rect(screen, ORANGE, score_rect)  # поле отображения очков в игре
+    show_text(screen, score, score_rect.x + 5, score_rect.y, 32, WHITE)  # очки
 
     screen.blit(car, car_rect)  # отображаю картинку с машиной
     screen.blit(tree1, tree1_rect)
@@ -118,6 +139,7 @@ while not finished:
         else:
             paddle.right = road.right  # третья дорожка
         paddle.y = -40  # поднять препятствие наверх
+        score += 1
     if paddle2.top >= H:  # если препятствие упало вниз
         x = r.randint(1, 3)  # сгенерировать случайное число
         if x == 1:
@@ -127,6 +149,7 @@ while not finished:
         else:
             paddle2.right = road.right  # третья дорожка
         paddle2.y = paddle.y - H // 2  # поднять препятствие наверх
+        score += 1
 
     tree1_rect.y += 10
     tree2_rect.y += 10
